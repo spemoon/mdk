000b9c00ea1339ca391aa15c00337f525079beda
caolvchong <caolvchong@gmail.com>
2012-03-28 18:33:29 +0800

    add util/core/bom/storage functions

--- js/api/util/core/bom/storage/app.js
+++ js/api/util/core/bom/storage/app.js
@@ -1,14 +1,31 @@
 define(function(require, exports, module) {
-	require('../../../../api_page.js');
-	var $ = require('../../../../../lib/jquery/1.7.1/sea_jquery.js');
+    require('../../../../api_page.js');
+    var $ = require('../../../../../lib/jquery/1.7.1/sea_jquery.js');
     var storage = require('../../../../../lib/util/core/bom/storage.js');
-	
+    
     $(function() {
         /**--------------------------------------------
-         * 实例一：
+         * 实例1： localStorage的存入，读取，删除，清空
          * --------------------------------------------*/
         (function() {
-
+            var btn1 = $('#btn1');
+            var btn2 = $('#btn2');
+            var btn3 = $('#btn3');
+            var btn4 = $('#btn4');
+            var txt1 = $('#txt1');
+			var txt2 = $('#txt2');
+            btn1.click(function() {
+                storage.set('val', txt1.val());
+            });
+            btn2.click(function() {
+                alert(storage.get('val'));
+            });
+			btn3.click(function() {
+                storage.set('word', txt2.val());
+            });
+            btn4.click(function() {
+                alert(storage.get('word'));
+            });
         })();
     });
 });
--- js/lib/util/core/bom/storage.js
+++ js/lib/util/core/bom/storage.js
@@ -1,3 +1,110 @@
 define(function(require, exports, module) {
     var $ = require('../../../jquery/1.7.1/sea_jquery.js');
+    /**
+     * localStorage：标准本地存储，IE8+，FF3.0+，Opera10.5+，Chrome4+，Safari4+，iPhone2+，Android2+
+     * 因此标准浏览器都支持，对于不支持的IE6，7使用IE特有的UserData来支持，其他低版本浏览器使用cookie支持
+     *
+     * localStorage限制：
+     *     一般为5MB，UserData通常不能超过1M，单文件不超过64KB
+     *     时间不受限制
+     *
+     * localStorage标准支持以下方法：
+     *     setItem(key, value)：设置一个本地存储
+     *     getItem(key)：获取对应存储
+     *     removeItem(key)：移除对应存储
+     *     key(i)：遍历所有localStorage
+     * 支持以下属性：
+     *     length：存放的key的数量
+     * 支持以下事件：
+     *     storage：当某个key的值有改变或者被删除时候会通知其他窗体响应的事件
+     *         由于模拟的UserData可以通过定时器来处理，并且判断值和当前窗体值不一样来触发事件
+     *         但这种实现相对来说麻烦，因此目前此事件不太适合来做兼容性的处理
+     *
+     * 用途：
+     *     1. 本地数据保存，即便用户关掉浏览器下次访问还可以还原临时数据，常用在表单临时保存。但涉及用户相关的最好做些加密操作
+     *     2. 利用storage事件达到窗体之间的通知
+     */
+    var storage = window.localStorage ||
+    (function() {
+        var node = document.createElement('input');
+        var api;
+        if (node.addBehavior) {
+            var isInit = false;
+            var init = function() {
+                if (!isInit) {
+                    try {
+                        var expires = new Date();
+                        node.type = 'hidden';
+                        node.addBehavior('#default#userData');
+                        document.body.appendChild(node);
+                        expires.setDate(expires.getDate() + 3650);
+                        node.expires = expires.toUTCString();
+                        isInit = true;
+                    } catch (e) {
+                        return false;
+                    }
+                }
+                return true;
+            };
+            var dataFileName = location.hostname;
+            api = {
+                setItem: function(key, value) {
+                    if (init()) {
+                        node.load(dataFileName);
+                        node.setAttribute(key, value);
+                        node.save(dataFileName);
+                    }
+                },
+                getItem: function(key) {
+                    var value = '';
+                    if (init()) {
+                        node.load(dataFileName);
+                        value = node.getAttribute(key);
+                    }
+                    return value;
+                },
+                removeItem: function(key) {
+                    if (init()) {
+                        node.load(dataFileName);
+                        node.removeAttribute(key);
+                        node.save(dataFileName);
+                    }
+                }
+            };
+        } else { // 使用cookie
+        alert(3);
+            var cookie = require('./cookie.js');
+            node = null;
+            api = {
+                setItem: function(key, value) {
+                    cookie.set(key, value, 3650 * 24 * 3600 * 1000);
+                },
+                getItem: function(key) {
+                    return cookie.get(key);
+                },
+                removeItem: function(key) {
+                    cookie.del(key);
+                }
+            };
+        }
+        api.clear = function() {
+        };
+        return api;
+    })();
+    
+    var r = {
+        set: function(key, value) {
+            storage.setItem(key, value);
+        },
+        get: function(key) {
+            return storage.getItem(key) || '';
+        },
+        del: function(key) {
+            storage.removeItem(key);
+        },
+        clear: function() {
+            storage.clear();
+        }
+    };
+    return r;
 });
