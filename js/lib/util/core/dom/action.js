304e7e783d16bbe059f582591d85a27f76fb155d
caolvchong <caolvchong@gmail.com>
2012-04-04 14:06:51 +0800

    增加事件代理模块，以便利用冒泡原理来处理子节点事件

---
+++ js/lib/util/core/dom/action.js
@@ -0,0 +1,51 @@
+define(function(require, exports, module) {
+    var $ = require('../../../jquery/1.7.1/sea_jquery.js');
+    var lang = require('../lang.js');
+
+    var r = {
+        /**
+         * 利用冒泡来做监听，这样做有以下优势：
+         *     1. 减少事件绑定数量，提高程序效率，尤其在列表性质的节点上，无需每个节点都绑定
+         *     2. 动态生成的内容无需绑定事件也能响应
+         *     3. 节点外点击隐藏某些节点
+         * @param actions {Object} 响应类型与对应处理事件，
+         *     键：是事件源上的data-action属性
+         *     值：可能是下面两种格式：
+         *         callback：回调函数，当事件源触发时候执行该函数，函数的this是事件源的jQuery节点，参数是事件对象
+         *         object：对象，包含两个属性：
+         *             is：回调函数，事件源触发时候执行，this是事件源的jQuery节点，参数是事件对象
+         *             not：点击在其他节点上时执行的回调函数，this是事件源的jQuery节点，参数是事件对象
+         * @param node {Object} jQuery对象，绑定的节点，是父容器
+         * @param type 事件类型，默认是click
+         * @return {object} jQuery对象，父节点
+         */
+        listener:function(actions, node, type) {
+            actions = actions || {};
+            node = node && node[0] ? node : $(document);
+            type = type || 'click';
+            node[type](function(e) {
+                var target = $(e.target);
+                var action = target.data('action') || target.parents('[data-action]').data('action');
+                var flag = true;
+                var fetch = actions[action];
+                if(fetch) {
+                    if(lang.isFunction(fetch)) {
+                        flag = fetch.call(target, e) === true;
+                    } else {
+                        if(lang.isFunction(fetch.is)) {
+                            flag = fetch.is.call(target, e) === true;
+                        }
+                    }
+                }
+                for(var key in actions) {
+                    if(key != action && actions[key] && actions[key].not && lang.isFunction(actions[key].not)) {
+                        actions[key].not.call(target, e);
+                    }
+                }
+                return flag;
+            });
+            return node;
+        }
+    };
+    return r;
+});
