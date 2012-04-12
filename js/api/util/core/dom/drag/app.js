9aaed8ecbf901ba0cb891c84e2102b47bf721295
caolvchong <caolvchong@gmail.com>
2012-04-13 00:04:00 +0800

    try to suppress click events after dragend, not finish

--- js/api/util/core/dom/drag/app.js
+++ js/api/util/core/dom/drag/app.js
@@ -1,25 +1,25 @@
 define(function(require, exports, module) {
-	require('../../../../api_page.js');
-	var $ = require('../../../../../lib/jquery/1.7.1/sea_jquery.js');
+    require('../../../../api_page.js');
+    var $ = require('../../../../../lib/jquery/1.7.1/sea_jquery.js');
     var drag = require('../../../../../lib/util/core/dom/drag.js');
-	
+
     $(function() {
         /**--------------------------------------------
          * 实例1：
          * --------------------------------------------*/
         (function() {
-            var drag1 = $('#drag1');
-            drag.reg(drag1);
+            drag.reg({
+                node:$('#drag1')
+            });
         })();
 
         /**--------------------------------------------
          * 实例2：
          * --------------------------------------------*/
         (function() {
-            var drag2 = $('#drag2');
-            var handle = drag2.children('.handle');
-            drag.reg(handle, {
-                node: drag2
+            drag.reg({
+                node:$('#drag2'),
+                handle:'.handle'
             });
         })();
 
@@ -27,10 +27,9 @@ define(function(require, exports, module) {
          * 实例3：
          * --------------------------------------------*/
         (function() {
-            var drag3 = $('#drag3');
-            var except3 = drag3.children('.except');
-            drag.reg(drag3, {
-                except: except3
+            drag.reg({
+                node:$('#drag3'),
+                except:'.except'
             });
         })();
 
@@ -38,10 +37,9 @@ define(function(require, exports, module) {
          * 实例4：
          * --------------------------------------------*/
         (function() {
-            var drag4 = $('#drag4');
-            var box1 = $('#box1');
-            drag.reg(drag4, {
-                container: box1
+            drag.reg({
+                node:$('#drag4'),
+                container:$('#box1')
             });
         })();
 
@@ -49,12 +47,12 @@ define(function(require, exports, module) {
          * 实例5：
          * --------------------------------------------*/
         (function() {
-            var drag5 = $('#drag5');
-            var drag6 = $('#drag6');
-            drag.reg(drag5, {
+            drag.reg({
+                node:$('#drag5'),
                 axisX:true
             });
-            drag.reg(drag6, {
+            drag.reg({
+                node:$('#drag6'),
                 axisY:true
             });
         })();
@@ -63,13 +61,13 @@ define(function(require, exports, module) {
          * 实例6：
          * --------------------------------------------*/
         (function() {
-            var drag7 = $('#drag7');
-            var drag8 = $('#drag8');
-            drag.reg(drag7, {
+            drag.reg({
+                node:$('#drag7'),
                 proxy:true,
-                dashed: true
+                dashed:true
             });
-            drag.reg(drag8, {
+            drag.reg({
+                node:$('#drag8'),
                 proxy:true
             });
         })();
@@ -78,8 +76,8 @@ define(function(require, exports, module) {
          * 实例8：
          * --------------------------------------------*/
         (function() {
-            var drag9 = $('#drag9');
-            drag.reg(drag9, {
+            drag.reg({
+                node:$('#drag9'),
                 scroll:true
             });
         })();
@@ -88,13 +86,18 @@ define(function(require, exports, module) {
          * 实例8：
          * --------------------------------------------*/
         (function() {
-			var box2 = $('#box2');
-            var drag10 = $('#drag10');
-			var drag11 = $('#drag11');
-			var drag12 = $('#drag12');
-			box2.find('.drag').click(function() {
-				$(this).toggleClass('selected');
-			});
+            var box2 = $('#box2');
+            var list = box2.find('.drag');
+            list.click(function() {
+                $(this).toggleClass('selected');
+            });
+            drag.reg({
+                node:list,
+                multi: function() {
+                    return box2.find('.selected');
+                }
+            });
+
         })();
     });
 });
--- js/lib/util/core/dom/drag.js
+++ js/lib/util/core/dom/drag.js
@@ -3,170 +3,183 @@ define(function(require, exports, module) {
     var lang = require('../lang.js');
     var array = require('../array.js');
     var mVar = require('./mVar.js');
-    
+
     var doc = $(document);
     var win = $(window);
     var status = 0; // 0: 初始或者mouseup时无拖拽，1: mousedown准备拖拽，2: 正在mousemove拖拽
     var helper = {};
     var r = {
         /**
-         * @param handle 拖拽句柄
          * @param params
          *        node: 被拖动节点
          *
          */
-        reg: function(handle, params) {
-            params = params || {};
-            var handle = $(handle);
-            var node = params.node ? $(params.node) : handle;
-            if (params.except) { // 拖拽句柄排除
-                params.except.css('cursor', 'default').mousedown(function(e) {
+        reg:function(params) {
+            var handle = params.handle ? params.node.find(params.handle) : params.node;
+            if(params.except) { // 拖拽句柄排除
+                params.node.find(params.except).css('cursor', 'default').mousedown(function(e) {
                     return false;
                 });
             }
-            handle.css('cursor', 'move').mousedown(function(e) {
-                if (e.which == 1) { // 限制左键拖动
-                    var zIndex = mVar.zIndex(); // 最高z-index
-                    var random = +new Date(); // 时间戳用来做事件命名空间
-                    var mouseX = e.pageX, mouseY = e.pageY;
-                    
-                    var container = $(params.container);
-                    var containerWidth = container.width();
-                    var containerHeight = container.height();
-                    
-                    var nodeList; // 一个或者多个被拖拽的节点，是个数组
-                    var startPosition = []; // 被拖拽节点的起始顶点坐标位置
-                    var nodeSize = []; // 被拖拽节点的宽高
-                    if (lang.isFunction(params.multi)) {
-                        nodeList = params.multi();
-                    } else {
-                        nodeList = [node];
-                    }
-                    array.forEach(function(v, i, arr) {
-                        var offset = v.offset();
-                        startPosition[i] = {
-                            x: offset.left,
-                            y: offset.top
-                        };
-                        nodeSize[i] = {
-                            width: v.outerWidth(),
-                            height: v.outerHeight()
-                        };
-                    }, nodeList);
-                    
-                    var action = {
-                        start: function(e) {
-                            status = 1;
-                            if (params.proxy) { // 代理容器节点生成
-                                var fragment = document.createDocumentFragment();
-                                proxyList = [];
-								array.forEach(function(v, i, arr) {
-									var css = {
-										position: 'absolute',
-                                        display: 'block',
-                                        left: startPosition[i].x,
-                                        top: startPosition[i].y,
-										width: nodeSize[i].width,
-										height: nodeSize[i].height,
-                                        zIndex: zIndex
-									};
-									var proxy = v.clone().css(css);
-									if (params.dashed) {
-										proxy.css('border', '1px dashed #555');
-										proxy[0].innerHTML = '';
-                                    }
-									proxyList[i] = proxy;
-									fragment.appendChild(proxy[0]);
-									
-                                }, nodeList);
-								document.body.appendChild(fragment);
-                            } else { // 非代理模式情况下将节点处理成absolute
-                                array.forEach(function(v, i, arr) {
-                                    $(v).css({
-                                        position: 'absolute',
-                                        left: startPosition[i].x,
-                                        top: startPosition[i].y,
-                                        zIndex: zIndex,
-                                        outline: 'none'
-                                    });
-                                }, nodeList);
-                                nodeList[0].attr('tabindex', '1').attr('hidefocus', 'true').focus();
+
+            handle.each(function(index, item) {
+                $(item).css('cursor', 'move').mousedown(function(e) {
+                    var scope = $(this);
+                    if(e.which == 1) { // 限制左键拖动
+                        var zIndex = mVar.zIndex(); // 最高z-index
+                        var random = +new Date(); // 时间戳用来做事件命名空间
+                        var mouseX = e.pageX, mouseY = e.pageY;
+
+                        var nodeList; // 一个或者多个被拖拽的节点，是个数组
+                        var startPosition = []; // 被拖拽节点的起始顶点坐标位置
+                        var nodeSize = []; // 被拖拽节点的宽高
+                        if(lang.isFunction(params.multi)) {
+                            nodeList = params.multi();
+                            if(nodeList && nodeList.length) {
+                                nodeList = [nodeList];
+                            } else {
+                                nodeList = [params.node.eq(index)];
                             }
-                            win.bind('blur.' + random, action.end); // 失去焦点时候触发mouseup，防止拖拽过程光标被其他程序抢夺后导致回到界面鼠标其实处于mouseup状态却可以拖拽);
-                            if (handle[0].setCapture) {
-                                handle[0].setCapture();
-                                handle.bind('losecapture.' + random, action.end);
-                            }
-                            if (params.scroll !== true) { // 拖动时候不影响滚动条，设置true则可以在拖动到边缘的同时让滚动条滚动
-                                e.preventDefault();
-                            }
-                        },
-                        drag: function(e) {
-                            if (status == 1 || status == 2) {
-                                if (status == 1) {
-                                    status = 2;
-                                }
-                                if (params.scroll === true) { // 拖动支持滚动条响应时候要清除文本选择（滚动条的运动应该就是文本选择导致的，设置禁止选择文本滚动条则不会响应拖拽）
-                                    window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
-                                }
-                                array.forEach(function(v, i, arr) {
-									var target = params.proxy ? proxyList[i] : v;
-									var minX, minY, maxX, maxY;
-									
-                                    if (container[0]) {
-                                        var containerOffset = container.offset();
-                                        minX = containerOffset.left;
-                                        maxX = minX + containerWidth - nodeSize[i].width;
-                                        minY = containerOffset.top;
-                                        maxY = minY + containerHeight - nodeSize[i].height;
-                                    } else {
-                                        minX = doc.scrollLeft();
-                                        maxX = minX + win.width() - nodeSize[i].width;
-                                        minY = doc.scrollTop();
-                                        maxY = minY + win.height() - nodeSize[i].height;
-                                    }
-                                    if (params.axisX) {
-                                        minY = maxY = startPosition[i].y;
-                                    }
-                                    if (params.axisY) {
-                                        minX = maxX = startPosition[i].x;
-                                    }
-                                    target.css({
-                                        left: Math.min(Math.max(e.pageX - mouseX + startPosition[i].x, minX), maxX),
-                                        top: Math.min(Math.max(e.pageY - mouseY + startPosition[i].y, minY), maxY)
-                                    });
-                                }, nodeList);
-                            }
-                        },
-                        end: function(e) {
-                            status = 0;
-                            if (params.proxy) {
-								array.forEach(function(v, i, arr) {
-									var offset = proxyList[i].offset();
-									$(v).css({
-	                                    top: offset.top,
-	                                    left: offset.left,
-	                                    position: 'absolute',
-	                                    visibility: 'visible',
-	                                    zIndex: zIndex
-	                                });
-									proxyList[i].remove();
-								}, nodeList);
-                            }
-                            if (lang.callback(params.drop)) {
-                                lang.callback(params.after);
-                            }
-                            if (handle[0].releaseCapture) {
-                                handle[0].releaseCapture();
-                            }
-                            doc.unbind('mousemove.' + random).unbind('mouseup.' + random);
-                            handle.unbind('losecapture.' + random);
-                            win.unbind('blur.' + random);
+                        } else {
+                            nodeList = [params.node.eq(index)];
                         }
-                    };
-                    action.start(e);
-                    doc.bind('mousemove.' + random, action.drag).bind('mouseup.' + random, action.end);
-                }
+                        array.forEach(function(v, i, arr) {
+                            var offset = v.offset();
+                            startPosition[i] = {
+                                x:offset.left,
+                                y:offset.top
+                            };
+                            nodeSize[i] = {
+                                width:v.outerWidth(),
+                                height:v.outerHeight()
+                            };
+                        }, nodeList);
+                        console.log(startPosition);
+
+                        var action = {
+                            start:function(e) {
+                                status = 1;
+                                if(params.proxy) { // 代理容器节点生成
+                                    var fragment = document.createDocumentFragment();
+                                    proxyList = [];
+                                    array.forEach(function(v, i, arr) {
+                                        var css = {
+                                            position:'absolute',
+                                            display:'block',
+                                            left:startPosition[i].x,
+                                            top:startPosition[i].y,
+                                            width:nodeSize[i].width,
+                                            height:nodeSize[i].height,
+                                            zIndex:zIndex
+                                        };
+                                        var proxy = v.clone().css(css);
+                                        if(params.dashed) {
+                                            proxy.css({
+                                                border:'1px dashed #555',
+                                                background:'transparent'
+                                            });
+                                            proxy[0].innerHTML = '';
+                                        }
+                                        proxyList[i] = proxy;
+                                        fragment.appendChild(proxy[0]);
+                                        v.css('visibility', 'hidden');
+                                    }, nodeList);
+                                    document.body.appendChild(fragment);
+                                } else { // 非代理模式情况下将节点处理成absolute
+                                    array.forEach(function(v, i, arr) {
+                                        v.css({
+                                            position:'absolute',
+                                            left:startPosition[i].x,
+                                            top:startPosition[i].y,
+                                            zIndex:zIndex,
+                                            outline:'none'
+                                        });
+                                    }, nodeList);
+                                    nodeList[0].attr('tabindex', '1').attr('hidefocus', 'true').focus();
+                                }
+                                win.bind('blur.' + random, action.end); // 失去焦点时候触发mouseup，防止拖拽过程光标被其他程序抢夺后导致回到界面鼠标其实处于mouseup状态却可以拖拽);
+                                if(scope[0].setCapture) {
+                                    scope[0].setCapture();
+                                    scope.bind('losecapture.' + random, action.end);
+                                }
+                                if(params.scroll !== true) { // 拖动时候不影响滚动条，设置true则可以在拖动到边缘的同时让滚动条滚动
+                                    e.preventDefault();
+                                }
+                            },
+                            drag:function(e) {
+                                if(status == 1 || status == 2) {
+                                    if(status == 1) {
+                                        status = 2;
+                                    }
+                                    if(params.scroll === true) { // 拖动支持滚动条响应时候要清除文本选择（滚动条的运动应该就是文本选择导致的，设置禁止选择文本滚动条则不会响应拖拽）
+                                        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+                                    }
+                                    array.forEach(function(v, i, arr) {
+                                        var target = params.proxy ? proxyList[i] : v;
+                                        var minX, minY, maxX, maxY;
+
+                                        if(params.container) {
+                                            var container = params.container;
+                                            var containerWidth = container.width();
+                                            var containerHeight = container.height();
+                                            var containerOffset = container.offset();
+                                            minX = containerOffset.left;
+                                            maxX = minX + containerWidth - nodeSize[i].width;
+                                            minY = containerOffset.top;
+                                            maxY = minY + containerHeight - nodeSize[i].height;
+                                        } else {
+                                            minX = doc.scrollLeft();
+                                            maxX = minX + win.width() - nodeSize[i].width;
+                                            minY = doc.scrollTop();
+                                            maxY = minY + win.height() - nodeSize[i].height;
+                                        }
+                                        if(params.axisX) {
+                                            minY = maxY = startPosition[i].y;
+                                        }
+                                        if(params.axisY) {
+                                            minX = maxX = startPosition[i].x;
+                                        }
+                                        target.css({
+                                            left:Math.min(Math.max(e.pageX - mouseX + startPosition[i].x, minX), maxX),
+                                            top:Math.min(Math.max(e.pageY - mouseY + startPosition[i].y, minY), maxY)
+                                        });
+                                    }, nodeList);
+                                }
+                            },
+                            end:function(e) {
+                                status = 0;
+                                if(params.proxy) {
+                                    array.forEach(function(v, i, arr) {
+                                        var offset = proxyList[i].offset();
+                                        v.css({
+                                            top:offset.top,
+                                            left:offset.left,
+                                            position:'absolute',
+                                            visibility:'visible',
+                                            zIndex:zIndex
+                                        });
+                                        proxyList[i].remove();
+                                    }, nodeList);
+                                }
+                                if(lang.callback(params.drop)) {
+                                    lang.callback(params.after);
+                                }
+                                if(scope[0].releaseCapture) {
+                                    scope[0].releaseCapture();
+                                }
+                                doc.unbind('mousemove.' + random).unbind('mouseup.' + random);
+                                scope.unbind('losecapture.' + random);
+                                win.unbind('blur.' + random);
+                                $.event.triggered = true;
+                                setTimeout(function() {
+                                    $.event.triggered = false;
+                                }, 20);
+                            }
+                        };
+                        action.start(e);
+                        doc.bind('mousemove.' + random, action.drag).bind('mouseup.' + random, action.end);
+                    }
+                });
             });
         }
     };
